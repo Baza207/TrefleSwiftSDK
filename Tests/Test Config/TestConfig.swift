4707e83883ff4b5ae2868b8de91e021795fabd13
James Barrow <baza207@me.com>
2020-04-21 13:42:54 +0200

    Added TestConfig files

---
+++ Tests/Test Config/TestConfig.swift
@@ -0,0 +1,36 @@
+//
+//  TestConfig.swift
+//  TrefleSwiftSDKTests
+//
+//  Created by James Barrow on 2020-04-21.
+//  Copyright © 2020 Pig on a Hill Productions. All rights reserved.
+//
+
+import Foundation
+
+private class TestConfigBundleClass { }
+
+struct TestConfig: Decodable {
+    
+    // MARK: - Properties
+    
+    let accessToken: String
+    
+    static func load(_ overrideUrl: URL? = nil) throws -> TestConfig? {
+        
+        let url: URL
+        if let overrideUrl = overrideUrl {
+            url = overrideUrl
+        } else if let defaultUrl = Bundle(for: TestConfigBundleClass.self).url(forResource: "TestConfig", withExtension: "json") {
+            url = defaultUrl
+        } else {
+            return nil
+        }
+        
+        let data = try Data(contentsOf: url)
+        
+        let decoder = JSONDecoder()
+        return try decoder.decode(TestConfig.self, from: data)
+    }
+    
+}
