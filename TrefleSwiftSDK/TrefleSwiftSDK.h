ea5a1d79016c5869e09ac6baa6baebe208f2a3fd
James Barrow <baza207@me.com>
2020-04-21 13:18:41 +0200

    Initial Commit

---
+++ TrefleSwiftSDK/TrefleSwiftSDK.h
@@ -0,0 +1,19 @@
+//
+//  TrefleSwiftSDK.h
+//  TrefleSwiftSDK
+//
+//  Created by James Barrow on 2020-04-21.
+//  Copyright © 2020 Pig on a Hill Productions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TrefleSwiftSDK.
+FOUNDATION_EXPORT double TrefleSwiftSDKVersionNumber;
+
+//! Project version string for TrefleSwiftSDK.
+FOUNDATION_EXPORT const unsigned char TrefleSwiftSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TrefleSwiftSDK/PublicHeader.h>
+
+
