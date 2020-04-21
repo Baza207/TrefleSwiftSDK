9f33e3d64ee10f9c80283c2361ed5151f25373f2
James Barrow <baza207@me.com>
2020-04-21 13:39:03 +0200

    Removed blank lines from template

--- Source/TrefleSwiftSDK.h
+++ Source/TrefleSwiftSDK.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double TrefleSwiftSDKVersionNumber;
 FOUNDATION_EXPORT const unsigned char TrefleSwiftSDKVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <TrefleSwiftSDK/PublicHeader.h>
-
-
