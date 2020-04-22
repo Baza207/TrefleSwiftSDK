938b331271bca49b93c3d89e62f419313adf2728
James Barrow <baza207@me.com>
2020-04-22 23:01:56 +0200

    Parse Page details

--- Source/Manager/Plants.swift
+++ Source/Manager/Plants.swift
@@ -10,7 +10,7 @@ import Foundation
 
 public class Plants {
     
-    public static func getPlants(query: String? = nil, completed: @escaping (Result<[PlantRef], Error>) -> Void) {
+    public static func getPlants(query: String? = nil, completed: @escaping (Result<Page<PlantRef>, Error>) -> Void) {
         
         guard let jwt = Trefle.shared.jwt else {
             completed(Result.failure(TrefleError.noJWT))
@@ -38,7 +38,7 @@ public class Plants {
         }
     }
     
-    private static func getPlantsURL(query: String?) -> URL? {
+    internal static func getPlantsURL(query: String?) -> URL? {
         
         guard var urlComponents = URLComponents(string: "\(Trefle.baseAPIURL)/plants") else {
             return nil
@@ -55,7 +55,7 @@ public class Plants {
         return urlComponents.url
     }
     
-    private static func getPlants(jwt: String, url: URL, completed: @escaping (Result<[PlantRef], Error>) -> Void) {
+    internal static func getPlants(jwt: String, url: URL, completed: @escaping (Result<Page<PlantRef>, Error>) -> Void) {
         
         let urlRequest = URLRequest.jsonRequest(url: url, jwt: jwt)
         let downloadTask = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
@@ -79,7 +79,15 @@ public class Plants {
                 return
             }
             
-            completed(Result.success(result ?? []))
+            guard
+                let response = response as? HTTPURLResponse,
+                let page = Page(items: result ?? [], response: response)
+            else {
+                completed(Result.failure(TrefleError.generalError))
+                return
+            }
+            
+            completed(Result.success(page))
         }
         downloadTask.resume()
     }
---
+++ Tests/PlantsTests.swift
@@ -0,0 +1,48 @@
+//
+//  PlantsTests.swift
+//  TrefleSwiftSDKTests
+//
+//  Created by James Barrow on 2020-04-22.
+//  Copyright © 2020 Pig on a Hill Productions. All rights reserved.
+//
+
+import XCTest
+@testable import TrefleSwiftSDK
+
+class PlantsTests: XCTestCase {
+    
+    var config = try? TestConfig.load()
+    
+    func testGetPlantRefs() throws {
+        
+        guard let config = self.config else {
+            XCTFail("Requires a test config to be setup before calling login!")
+            return
+        }
+        
+        guard let url = Plants.getPlantsURL(query: nil) else {
+            XCTFail("Failed to create URL!")
+            return
+        }
+        
+        let expectation = self.expectation(description: #function)
+        
+        Plants.getPlants(jwt: config.accessToken, url: url) { (result) in
+            
+            switch result {
+            case .success(let page):
+                XCTAssert(page.items.count > 0, "No returned items!")
+                
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+            }
+            
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 60) { (error) in
+            XCTAssertNil(error, error?.localizedDescription ?? "")
+        }
+    }
+    
+}
