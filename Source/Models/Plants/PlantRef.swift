d65afedf6941b085d9f355bc923043caac990eda
James Barrow <baza207@me.com>
2020-04-25 20:57:48 +0200

    Moved PlantRef out of Plants

--- Source/Models/Plants/PlantRef.swift
+++ Source/Models/Plants/PlantRef.swift
@@ -8,34 +8,30 @@
 
 import Foundation
 
-public extension Plants {
+public struct PlantRef: Decodable, CustomStringConvertible {
     
-    struct PlantRef: Decodable, CustomStringConvertible {
-        
-        // MARK: - Properties
-        
-        public let identifier: Int
-        public let slug: String
-        public let scientificName: String
-        public let commonName: String?
-        public let link: String
-        public let completeData: Bool
-        
-        public var description: String {
-            "PlantRef(identifier: \(identifier), slug: \(slug), scientificName: \(scientificName), commonName: \(commonName ?? "-"), link: \(link), completeData: \(completeData))"
-        }
-        
-        // MARK: - Coding
-        
-        private enum CodingKeys: String, CodingKey {
-            case identifier = "id"
-            case slug
-            case scientificName = "scientific_name"
-            case commonName = "common_name"
-            case link
-            case completeData = "complete_data"
-        }
-        
+    // MARK: - Properties
+    
+    public let identifier: Int
+    public let slug: String
+    public let scientificName: String
+    public let commonName: String?
+    public let link: String
+    public let completeData: Bool
+    
+    public var description: String {
+        "PlantRef(identifier: \(identifier), slug: \(slug), scientificName: \(scientificName), commonName: \(commonName ?? "-"), link: \(link), completeData: \(completeData))"
+    }
+    
+    // MARK: - Coding
+    
+    private enum CodingKeys: String, CodingKey {
+        case identifier = "id"
+        case slug
+        case scientificName = "scientific_name"
+        case commonName = "common_name"
+        case link
+        case completeData = "complete_data"
     }
     
 }
