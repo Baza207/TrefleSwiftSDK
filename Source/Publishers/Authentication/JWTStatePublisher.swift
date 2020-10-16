393fdc2e27dc2150a917d80ba2bafd9d63b6a3a1
James Barrow <baza207@me.com>
2020-10-16 22:17:40 +0200

    Updated publishers for JWTState, ResponseList and ResponseItem to work the same as operations

--- Source/Publishers/Authentication/JWTStatePublisher.swift
+++ Source/Publishers/Authentication/JWTStatePublisher.swift
@@ -9,14 +9,36 @@
 import Foundation
 import Combine
 
-@available(iOS 13, *)
-extension JWTState {
+internal extension JWTState {
     
-    static func publisher(_ urlRequest: URLRequest) -> AnyPublisher<JWTState, Error> {
-        URLSession.shared.dataTaskPublisher(for: urlRequest)
-            .map(\.data)
-            .decode(type: JWTState.self, decoder: JSONDecoder.jwtJSONDecoder)
+    static func publisher() -> AnyPublisher<JWTState, Error> {
+        
+        if Trefle.shared.isValid == true {
+            
+            return Future<JWTState, Error> { (promise) in
+                if let jwtState = Trefle.shared.jwtState {
+                    promise(.success(jwtState))
+                } else {
+                    promise(.failure(TrefleError.noJWT))
+                }
+            }
             .eraseToAnyPublisher()
+        }
+        
+        return Future<URLRequest, Error> { promise in
+            if let urlRequest = JWTState.urlRequest {
+                promise(.success(urlRequest))
+            }
+            promise(.failure(TrefleError.badURL))
+        }
+        .flatMap { (urlRequest) -> AnyPublisher<JWTState, Error> in
+            
+            URLSession.shared.dataTaskPublisher(for: urlRequest)
+                .map(\.data)
+                .decode(type: JWTState.self, decoder: JSONDecoder.jwtJSONDecoder)
+                .eraseToAnyPublisher()
+        }
+        .eraseToAnyPublisher()
     }
     
 }
